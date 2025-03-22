8b5bac48553cab6625619fd39f26d7b16edea9fc
Krone <ahustinkrone@gmail.com>
2025-03-22 02:45:56 -0300

    Add more cases to test shader

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 const canvas = document.querySelector(".glslCanvas");
-canvas.width = 300;
-canvas.height = 300;
+canvas.width = document.body.clientWidth;
+canvas.height = document.body.clientHeight;
 const sandbox = new GlslCanvas(canvas);
+let previousCursor = { x: 0, y: 0, z: 10, w: 20 };
+let currentCursor = { x: 0, y: 0, z: 10, w: 20 };
+let option = 0;
+
 fetch("/shaders/cursor_example.glsl")
   .then((response) => {
     return response.text();
   })
   .then((fragment) => {
     sandbox.load(fragment);
+    gameLoop();
   });
 
-let previousCursor = { x: 0, y: 1, z: 20, w: 40 };
-let currentCursor = { x: 0, y: 1, z: 20, w: 40 };
-
-updateCursor();
-updateCursor();
 function setCursorUniforms() {
   sandbox.setUniform(
     "iCursorCurrent",
@@ -32,21 +32,80 @@ function setCursorUniforms() {
   );
   sandbox.setUniform("iTimeCursorChange", performance.now() / 1000);
 }
-
 function updateCursor() {
-  // Update the previous cursor to current
   previousCursor = { ...currentCursor };
-
-  // Simulate new cursor position (for example, setting a new random position)
-  const z = 20;
-  const w = 40;
+  const z = 10;
+  const w = 20;
   const x = Math.random() * canvas.width;
   const y = Math.random() * canvas.height;
-
   currentCursor = { x: x, y: y, z: z, w: w };
-  console.log(currentCursor);
-
+  setCursorUniforms();
+}
+function moveCursor(x, y) {
+  y = canvas.height - y;
+  previousCursor = { ...currentCursor };
+  currentCursor = {
+    x: x,
+    y: y,
+    z: 10,
+    w: 20,
+  };
   setCursorUniforms();
 }
 
-setInterval(updateCursor, 3000); // Change every 10 seconds
+canvas.addEventListener("click", function () {
+  change(1);
+});
+canvas.addEventListener("contextmenu", function (event) {
+  event.preventDefault(); // Prevent the default context menu from appearing
+  change(-1);
+});
+
+function gameLoop() {
+  change(1);
+  // setInterval(updateCursor, 3000); // Change every 10 seconds
+}
+
+function change(value) {
+  let top = canvas.height * 0.1;
+  let bottom = canvas.height * 0.9;
+  let left = canvas.width * 0.1;
+  let right = canvas.width * 0.9;
+
+  option = (option + value) % 8;
+  console.log(option);
+  switch (option) {
+    case 0:
+      moveCursor(left, top);
+      moveCursor(right, bottom);
+      break;
+    case 1:
+      moveCursor(left, bottom);
+      moveCursor(right, top);
+      break;
+    case 2:
+      moveCursor(right, bottom);
+      moveCursor(left, top);
+      break;
+    case 3:
+      moveCursor(right, top);
+      moveCursor(left, bottom);
+      break;
+    case 4:
+      moveCursor(top, right);
+      moveCursor(bottom, right);
+      break;
+    case 5:
+      moveCursor(bottom, right);
+      moveCursor(top, right);
+      break;
+    case 6:
+      moveCursor(bottom, left);
+      moveCursor(bottom, right);
+      break;
+    case 7:
+      moveCursor(top, right);
+      moveCursor(top, left);
+      break;
+  }
+}
