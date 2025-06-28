e3bdf1bb25563354e5c4c2445923a28491708d19
utensil <utensilcandel@gmail.com>
2025-06-28 21:48:50 +0800

    fix: disable lightning effect in cursor shader

--- shaders/cursor_blaze_lightning.glsl
+++ shaders/cursor_blaze_lightning.glsl
@@ -325,7 +325,7 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord)
     // Lightning activation check
     float delta_x = curr_pos.x - prev_pos.x;
     float delta_y = abs(curr_pos.y - prev_pos.y);
-    bool should_lightning = delta_x > 1.0 && delta_y < 5.0;
+    bool should_lightning = false; //true; // delta_x > 0.0;
 
     if (progress < 1.0) {
         vec2 trailAxis = v3 - v0;
